3b8243e94f2e318eda480f5bb4f51a8832c25aeb
Aiden Cullo <aidencullo@gmail.com>
2025-09-09 13:22:04 -0400

    Update App.vue

--- vue_app/src/App.vue
+++ vue_app/src/App.vue
@@ -1,15 +1,20 @@
 <script setup>
+import { ref } from 'vue';
+
 const handleClick = () => {
-  paras.push('This is the fourth paragraph');
+  paras.value.push(newPara.value);
+  newPara.value = '';
 }
-const paras = []
+const paras = ref([])
+const newPara = ref('');
 </script>
 
 <template>
   <div>
-<button @click="handleClick">Click me</button>
+<input type="text" v-model="newPara" @keyup.enter="handleClick"></input>
 </div>
 <div>
+  <h1>Paras</h1>
   <p v-for="(text, i) in paras" :key="i">{{ text }}</p>
 </div>
 </template>
