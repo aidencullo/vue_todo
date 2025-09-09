91718151f072f4057ec12692f6ec6d0a8432b362
Aiden Cullo <aidencullo@gmail.com>
2025-09-09 13:11:45 -0400

    Initial commit: Vue.js todo app setup

---
+++ vue_app/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+const handleClick = () => {
+  paras.push('This is the fourth paragraph');
+}
+const paras = []
+</script>
+
+<template>
+  <div>
+<button @click="handleClick">Click me</button>
+</div>
+<div>
+  <p v-for="(text, i) in paras" :key="i">{{ text }}</p>
+</div>
+</template>
